50755a5338578b7674cebfe4d51820c9f75ff2a2
Maximilian Thiel <maximilianthiel@maximilians-mbp.myfiosgateway.com>
2022-04-01 13:19:32 -0400

    Created a homepage at / route and has home button with id order-pizza

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React from "react";
+import { Route, Link } from "react-router-dom"
 
 const App = () => {
   return (
     <>
-      <h1>Lambda Eats</h1>
-      <p>You can remove this code and create your own header</p>
+      <header>
+        <h1>Lambda Eats</h1>
+        <p>You can remove this code and create your own header</p>
+        <nav id="order-pizza">
+          <Link to="/">Home</Link>
+        </nav>
+      </header>
+      
+
     </>
   );
 };
